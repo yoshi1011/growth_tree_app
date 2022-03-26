ee779572f298d47b7e9eec5a6d5d4c2982d657d8
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2022-03-26 19:17:27 +0900

    新規登録画面で今後共通化できる部分をフレーム化

---
+++ lib/widgets/frame/auth_page_frame.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:growth_tree_app/utils/colors.dart';
+
+class AuthPageFrame extends HookConsumerWidget {
+  final String title;
+  final Widget body;
+
+  const AuthPageFrame({Key? key, required this.title, required this.body})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 340,
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              SvgPicture.asset(
+                'assets/images/logo.svg',
+                semanticsLabel: 'Logo',
+                height: 70,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SelectableText(
+                title,
+                style: const TextStyle(
+                    color: GrowthTreeColors.themeColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              body
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
