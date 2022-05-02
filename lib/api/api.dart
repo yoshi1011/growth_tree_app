18c165b3b798f0d3c26c95e692810aa2233ef797
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2022-05-02 17:45:52 +0900

    feat: セッション情報をCookie管理に変更(Set-Cookieを利用) GT-13

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,43 +1,21 @@
+import 'package:dio/adapter_browser.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-import '../utils/constants.dart';
 
 class ApiMethod {
   static Future<dynamic> get(String params) async {
     var dio = Dio(
       BaseOptions(baseUrl: 'http://localhost:3000/api/v1'),
     );
-
-    var storage = const FlutterSecureStorage();
-    final accessToken = await storage.read(key: Constant.accessToken);
-    final client = await storage.read(key: Constant.client);
-    final expiry = await storage.read(key: Constant.expiry);
-    final uid = await storage.read(key: Constant.uid);
+    final clientAdapter = BrowserHttpClientAdapter();
+    clientAdapter.withCredentials = true;
+    dio.httpClientAdapter = clientAdapter;
 
     Options options = Options(
       responseType: ResponseType.json,
-      headers: {
-        'token-type': 'Bearer',
-        'access-token': accessToken,
-        'client': client,
-        'expiry': expiry,
-        'uid': uid
-      },
     );
 
     var result = await dio.get(params, options: options).then(
       (response) async {
-        Headers headers = response.headers;
-        // access-tokenは毎回更新されるため、
-        // headersに含まれている(有効な通信が得られた)場合は再セットする
-        if (headers['access-token'] != null) {
-          await storage.write(
-            key: Constant.accessToken,
-            value: headers['access-token']![0],
-          );
-        }
-
         return response.data;
       },
     ).catchError(
@@ -59,34 +37,16 @@ class ApiMethod {
       BaseOptions(baseUrl: 'http://localhost:3000/api/v1'),
     );
 
-    var storage = const FlutterSecureStorage();
-    final accessToken = await storage.read(key: Constant.accessToken);
-    final client = await storage.read(key: Constant.client);
-    final expiry = await storage.read(key: Constant.expiry);
-    final uid = await storage.read(key: Constant.uid);
+    final clientAdapter = BrowserHttpClientAdapter();
+    clientAdapter.withCredentials = true;
+    dio.httpClientAdapter = clientAdapter;
 
     Options options = Options(
       responseType: ResponseType.json,
-      headers: {
-        'token-type': 'Bearer',
-        'access-token': accessToken,
-        'client': client,
-        'expiry': expiry,
-        'uid': uid
-      },
     );
 
     var result = dio.post(params, options: options, data: body).then(
       (response) async {
-        Headers headers = response.headers;
-        // access-tokenは毎回更新されるため、
-        // headersに含まれている(有効な通信が得られた)場合は再セットする
-        if (headers['access-token'] != null) {
-          await storage.write(
-            key: Constant.accessToken,
-            value: headers['access-token']![0],
-          );
-        }
 
         return response.data;
       },
@@ -108,35 +68,16 @@ class ApiMethod {
     var dio = Dio(
       BaseOptions(baseUrl: 'http://localhost:3000/api/v1'),
     );
-
-    var storage = const FlutterSecureStorage();
-    final accessToken = await storage.read(key: Constant.accessToken);
-    final client = await storage.read(key: Constant.client);
-    final expiry = await storage.read(key: Constant.expiry);
-    final uid = await storage.read(key: Constant.uid);
+    final clientAdapter = BrowserHttpClientAdapter();
+    clientAdapter.withCredentials = true;
+    dio.httpClientAdapter = clientAdapter;
 
     Options options = Options(
       responseType: ResponseType.json,
-      headers: {
-        'token-type': 'Bearer',
-        'access-token': accessToken,
-        'client': client,
-        'expiry': expiry,
-        'uid': uid
-      },
     );
 
     var result = dio.patch(params, options: options, data: body).then(
           (response) async {
-        Headers headers = response.headers;
-        // access-tokenは毎回更新されるため、
-        // headersに含まれている(有効な通信が得られた)場合は再セットする
-        if (headers['access-token'] != null) {
-          await storage.write(
-            key: Constant.accessToken,
-            value: headers['access-token']![0],
-          );
-        }
 
         return response.data;
       },
@@ -158,35 +99,16 @@ class ApiMethod {
     var dio = Dio(
       BaseOptions(baseUrl: 'http://localhost:3000/api/v1'),
     );
-
-    var storage = const FlutterSecureStorage();
-    final accessToken = await storage.read(key: Constant.accessToken);
-    final client = await storage.read(key: Constant.client);
-    final expiry = await storage.read(key: Constant.expiry);
-    final uid = await storage.read(key: Constant.uid);
+    final clientAdapter = BrowserHttpClientAdapter();
+    clientAdapter.withCredentials = true;
+    dio.httpClientAdapter = clientAdapter;
 
     Options options = Options(
       responseType: ResponseType.json,
-      headers: {
-        'token-type': 'Bearer',
-        'access-token': accessToken,
-        'client': client,
-        'expiry': expiry,
-        'uid': uid
-      },
     );
 
     var result = await dio.delete(params, options: options).then(
       (response) async {
-        Headers headers = response.headers;
-        // access-tokenは毎回更新されるため、
-        // headersに含まれている(有効な通信が得られた)場合は再セットする
-        if (headers['access-token'] != null) {
-          await storage.write(
-            key: Constant.accessToken,
-            value: headers['access-token']![0],
-          );
-        }
 
         return response.data;
       },
